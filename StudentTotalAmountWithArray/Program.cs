a0ed6e3a47ea90d06e715a8241194d46132e7dbc
paras Zalariya <paraszalariya12@gmail.com>
2020-10-03 17:23:42 +0530

    Count Of Student Found With Studentt Name

---
+++ StudentTotalAmountWithArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTotalAmountWithArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type Array With Name and Marks Combinations:");
+            String inputvalues = Convert.ToString(Console.ReadLine());
+            String[] values = inputvalues.Split(',');
+            var result = values.ToList();
+
+            var objects = result.Where(x => !string.IsNullOrEmpty(x)).Select(x => x.Split(':')).GroupBy(a => a[0].Trim()).Select(a => new { name = a.Key + " " + Convert.ToString(a.ToList().Sum(s => Convert.ToDouble(s[1]))) });
+            var finalResult = objects.Select(a => a.name).ToArray();
+            var abc = string.Join(",", finalResult);
+
+            Console.WriteLine(abc);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StudentTotalAmountWithArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StudentTotalAmountWithArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StudentTotalAmountWithArray")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6942e8b1-4340-4ef5-8aa0-400fa68a8da1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
